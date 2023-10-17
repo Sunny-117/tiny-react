18a63ef143195c9785cd9127c7a27e200e5fad84
fuzhiqiang <fuzhiqiang@baidu.com>
2023-10-17 10:41:47 +0800

    test: useEffect

--- scripts/jest/jest.config.js
+++ scripts/jest/jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
 	...defaults,
 	rootDir: process.cwd(),
 	modulePathIgnorePatterns: ['<rootDir>/.history'],
+	moduleNameMapper: {
+		'^scheduler$': '<rootDir>/node_modules/scheduler/unstable_mock.js'
+	},
 	moduleDirectories: [
 		// 对于react reactdom
 		// 先从dist/node_modules下面解析
@@ -11,5 +14,10 @@ module.exports = {
 		// 对于第三方依赖
 		...defaults.moduleDirectories
 	],
+	fakeTimers: {
+		enableGlobally: true,
+		legacyFakeTimers: true
+	},
+	setupFilesAfterEnv: ['./scripts/jest/setupJest.js'],
 	testEnvironment: 'jsdom'
 };
